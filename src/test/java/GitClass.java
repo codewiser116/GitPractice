c2bd19050caa62869cb3659c1f5e5df231ff5a4c
mariiazatylna <mariiazatylna@gmail.com>
2023-10-12 20:08:36 +0200

    hjhhh

--- src/test/java/GitClass.java
+++ src/test/java/GitClass.java
@@ -1,11 +1,14 @@
 public class GitClass {
 
     // we will make chages here -> Aika
+
+
+    //juiu
+
     //added changed2 -> Aika
     //added changed 3 -> Aika
     //added line 4-> Aika
     //added line 5 ->Aika
-    //
     //added line6 ->
 
 }
